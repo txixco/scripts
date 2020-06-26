01e57e0507ea34f88743a3a5f7b1d2456b6f511c
Francisco Rueda <fjrueda@west.com>
2020-06-26 10:22:16 -0500

    Modified to have two vertical panes

--- rsession.sh
+++ rsession.sh
@@ -4,8 +4,11 @@ tmux has-session -t $1
 if [ $? != 0 ]
 then
     tmux new -s $1 -n dunning-letters -c Codigo/UiPath/finance---dunning-letters -d
-    tmux new-window -n library -c Codigo/UiPath/application-libraries
-    tmux new-window -n man "man tmux"
+    tmux split-window -h -c Codigo/UiPath/application-libraries
+    tmux select-pane -t {left}
+ 
+#    tmux new-window -n library -c Codigo/UiPath/application-libraries
+#    tmux new-window -n man "man tmux"
 
     tmux select-window -t $1:dunning-letters
 fi

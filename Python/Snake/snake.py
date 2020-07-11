6c5e2948b7a191682fb223df174690b36d637abb
Francisco Rueda <fruedadev@gmail.com>
2020-07-11 15:17:51 -0500

    Commit before going on with another thing

--- Python/Snake/snake.py
+++ Python/Snake/snake.py
@@ -8,10 +8,11 @@ import sys
 import random
 import pygame
 
-from pygame.locals import QUIT
+from pygame.locals import QUIT, K_LEFT, K_RIGHT
 
 # Initialization
 pygame.init()
+DISPLAYSURF = pygame.display.set_mode((300, 300))
 
 # Colors
 BLUE = (0, 0, 255)
@@ -19,6 +20,11 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+# Others
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
 
 # FPS
 FPS = 60
@@ -30,13 +36,14 @@ DISPLAY_SURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
 # Classes
+
 class Enemy(pygame.sprite.Sprite):
     """
     Class to represent the enemy
     """
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("enemy.png")
+        self.image = pygame.image.load("Enemy.png")
         self.surf = pygame.Surface((50, 80))
         self.rect = self.surf.get_rect(center=(random.randint(40, 360), 0))
 
@@ -56,21 +63,54 @@ class Enemy(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
 
-# Creating Lines and Shapes
-pygame.draw.line(DISPLAY_SURF, BLUE, (150, 130), (130, 170))
-pygame.draw.line(DISPLAY_SURF, BLUE, (150, 130), (170, 170))
-pygame.draw.line(DISPLAY_SURF, GREEN, (130, 170), (170, 170))
-pygame.draw.circle(DISPLAY_SURF, BLACK, (100, 50), 30)
-pygame.draw.circle(DISPLAY_SURF, BLACK, (200, 50), 30)
-pygame.draw.rect(DISPLAY_SURF, RED, (100, 200, 100, 50), 2)
-pygame.draw.rect(DISPLAY_SURF, BLACK, (110, 260, 80, 5))
+class Player(pygame.sprite.Sprite):
+    """
+    Class for the player
+    """
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.surf = pygame.Surface((50, 100))
+        self.rect = self.surf.get_rect()
 
-# Game loop
+    def update(self):
+        """
+        Update the screen
+        """
+        pressed_keys = pygame.key.get_pressed()
+       #if pressed_keys[K_UP]:
+            #self.rect.move_ip(0, -5)
+       #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(0,5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        """
+        Draw the screen
+        """
+        surface.blit(self.image, self.rect)
+
+
+P1 = Player()
+E1 = Enemy()
+
 while True:
-    pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    P1.update()
+    E1.move()
 
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+    E1.draw(DISPLAYSURF)
+
+    pygame.display.update()
     frame_per_sec.tick(FPS)
--- Python/renumerar.py
+++ Python/renumerar.py
@@ -1,48 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding:  iso-8859-15 -*-
- 
-'''
+
+"""
 Renumera los archivos de un directorio, generalmente imágenes.
-'''
- 
-import sys
+"""
+
+import argparse
 from glob import glob
 from os import rename
- 
-def getDigits(src) :
-    try :
-        digits = int(src)
-    except ValueError :
-        print('El tercer argumento debe ser un número')
-        exit(1)
- 
-    if (digits <= 0) :
-        print('El número de dígitos debe ser mayor que 0')
-        exit(1)
- 
-    return digits
- 
-if (len(sys.argv) < 3) :
-    print('El número de argumentos no es válido, deben ser 3')
+
+# Get the options
+parser = argparse.ArgumentParser(description='Renumber the files specified by EXTension')
+
+parser.add_argument('STRING', help='String to be used for the filenames')
+parser.add_argument('EXTENSION', help='The EXTension of the files to renumber')
+parser.add_argument('DIGITS', help='Digits to be used for numbering the files')
+
+args = parser.parse_args()
+DIGITS = int(args.DIGITS)
+STRING = args.STRING
+EXT = args.EXTENSION
+
+if DIGITS <= 0:
+    print("El número de dígitos debe ser mayor que 0")
     exit(1)
- 
-cadena = sys.argv[1]
-ext = sys.argv[2]
-digits = getDigits(sys.argv[3])
- 
-cents = 10 ** digits
-maxNumber = int('9' * digits)
-num = 0
-i = 1
- 
-files = glob('*.' + ext)
-if (len(files) > maxNumber) :
-    print('La cantidad de archivos excede el número de dígitos')
-    exit(1)
- 
-for i, file in enumerate(files) :
-    num = str(cents + i + 1)
-    num = num[1:digits+1]
-    newFile = '{}-{}.{}'.format(cadena, num, ext)
-    print('Renaming {} to {}'.format(file, newFile))
-    rename(file, newFile)
+
+def main():
+    """
+    Main function, where the magic happens
+    """
+
+    cents = 10 ** DIGITS
+    max_number = int("9" * DIGITS)
+    num = 0
+    i = 1
+
+    files = glob("*." + EXT)
+    if len(files) > max_number:
+        print("La cantidad de archivos excede el número de dígitos")
+        exit(1)
+
+    for i, file in enumerate(files):
+        num = str(cents + i + 1)
+        num = num[1:DIGITS+1]
+        new_file = "{}-{}.{}".format(STRING, num, EXT)
+        print("Renaming {} to {}".format(file, new_file))
+        rename(file, new_file)
+
+# Let's go
+main()

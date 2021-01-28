b4c8ad4c2db3af6c321fcd97afea54d300b97b82
Francisco Rueda <fjrueda@west.com>
2021-01-28 09:59:09 -0600

    Modifications to rsession.sh

--- rsession.sh
+++ rsession.sh
@@ -4,7 +4,7 @@ tmux has-session -t $1
 if [ $? != 0 ]
 then
     tmux new -s $1 -n dunning-letters -c Code/UIPath -d cmatrix
-    tmux split-window -h -c Code/UIPath/finance---dunning-letters -p 80
+    tmux split-window -h -c Code/UIPath -p 90
     tmux split-window -v -c Code/UIPath/application-libraries
  
     tmux select-window -t $1:dunning-letters

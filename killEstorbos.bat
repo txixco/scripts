2a545f460a87af505654479f7680ce903603616d
Francisco Rueda <fjrueda@west.com>
2021-03-08 08:22:15 -0600

    Added a bat for killing some apps at a time

---
+++ killEstorbos.bat
@@ -0,0 +1,3 @@
+CALL C:\Scripts\killall.bat edge.exe
+CALL C:\Scripts\killall.bat ShellExperienceHost.exe
+CALL C:\Scripts\killall.bat WindowsInternal.ComposableShell.Experiences.TextInput.InputApp.exe

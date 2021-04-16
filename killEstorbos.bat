c70a85f36e0befb451dfacfd76c7095476158f51
Francisco Rueda <fjrueda@west.com>
2021-04-16 17:02:23 -0500

    Some modifications

--- killEstorbos.bat
+++ killEstorbos.bat
@@ -1,3 +1,5 @@
 CALL C:\Scripts\killall.bat edge.exe
 CALL C:\Scripts\killall.bat ShellExperienceHost.exe
 CALL C:\Scripts\killall.bat WindowsInternal.ComposableShell.Experiences.TextInput.InputApp.exe
+CALL C:\Scripts\killall.bat SystemSettings.exe
+CALL C:\Scripts\killall.bat WinStore.App.exe

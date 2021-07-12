7d81fa6bc975eebae5142d4740fc38f17a00fc94
Francisco Rueda <fruedadev@gmail.com>
2021-07-12 17:04:02 -0500

    Solved the Project Euler's Problem 10

--- Python/euler.py
+++ Python/euler.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timedelta
+from mathutils import isPrime
 
-FSUM = 1000
-
-def triplet(n):
-    for a in range(1, n // 3):
-        for b in range(a+1, (n - a) // 2):
-            c = n - a - b
-            if (a**2 + b**2 == c**2):
-                return (a, b, c)
+MAXNUMBER = 2_000_000
 
 print()
-print(f"Getting the Pythagorean Triplet for which a+b+c = 1000")
+print(f"Getting the sum of the primes below {MAXNUMBER}")
 start = datetime.now()
 
-(a, b, c) = triplet(FSUM)
+primes = [ i for i in range(2, MAXNUMBER+1) if isPrime(i) ]
 
-print(f"The triplet is {(a, b, c)}")
-print(f"The product is: {a * b * c}")
+print(f"The sum is {sum(primes)}")
 print(f"Elapsed time: {datetime.now() - start} ")
 print()

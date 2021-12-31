5653dc63f8bd34070f2558d84b5202542255bcdb
Francisco Rueda <fruedadev@gmail.com>
2021-12-31 16:24:04 -0600

    Refactoring on Python/renumerar.py

--- Python/renumerar.py
+++ Python/renumerar.py
@@ -1,48 +1,45 @@
 #!/usr/bin/env python3
-# -*- coding:  iso-8859-15 -*-
  
 '''
-Renumera los archivos de un directorio, generalmente imágenes.
+Renum the files in a directory
 '''
  
-import sys
+import sys, argparse
+
 from glob import glob
 from os import rename
- 
-def getDigits(src) :
-    try :
-        digits = int(src)
-    except ValueError :
-        print('El tercer argumento debe ser un número')
-        exit(1)
- 
-    if (digits <= 0) :
-        print('El número de dígitos debe ser mayor que 0')
-        exit(1)
- 
-    return digits
- 
-if (len(sys.argv) < 3) :
-    print('El número de argumentos no es válido, deben ser 3')
-    exit(1)
- 
-cadena = sys.argv[1]
-ext = sys.argv[2]
-digits = getDigits(sys.argv[3])
- 
-cents = 10 ** digits
-maxNumber = int('9' * digits)
-num = 0
-i = 1
- 
-files = glob('*.' + ext)
-if (len(files) > maxNumber) :
-    print('La cantidad de archivos excede el número de dígitos')
-    exit(1)
- 
-for i, file in enumerate(files) :
-    num = str(cents + i + 1)
-    num = num[1:digits+1]
-    newFile = '{}-{}.{}'.format(cadena, num, ext)
-    print('Renaming {} to {}'.format(file, newFile))
-    rename(file, newFile)
+
+def getArgs():
+    parser = argparse.ArgumentParser("Renumerate, given an extension, the files in the current directory")
+    parser.add_argument("-s", "--string", dest="string", required=True, help="the prefix for the file names")
+    parser.add_argument("-e", "--extension", dest="ext", required=True, help="the extension to renumerate")
+    parser.add_argument("-d", "--digits", type=int, dest="digits", default=0, help="the number of digits for the order number")
+    parser.add_argument("-n", dest="nel", action="store_true", default=False, help="indicates if the renaming is NOT in effect")
+
+    return parser.parse_args()
+
+def main():
+    args = getArgs()
+    string = args.string
+    ext = args.ext
+    nel = args.nel
+    
+    files = glob("*." + ext)
+    files.sort()
+    digits = args.digits if args.digits>0 else len(str(len(files)))
+
+    if (len(files) > int("9" * (10 ** digits))) :
+        print("The files quantity is more than the alowed by the number of digits")
+        exit(2)
+
+    num = 0
+    i = 1
+    
+    for i, file in enumerate(files) :
+        newFile = f"{string}-{i+1:0{digits}}.{ext.lower()}"
+        print(f"Renaming {file} to {newFile}")
+        if (not nel):
+            rename(file, newFile)
+
+if __name__ == "__main__":
+    main()

f44950376cc251a28ba7c7cd9add25eab8b3f8d3
Francisco Rueda <fruedadev@gmail.com>
2022-04-03 18:45:20 -0500

    Modifications to inicial.sh

--- inicial.sh
+++ inicial.sh
@@ -7,15 +7,8 @@ sudo pacman -Syyu
 ~/scripts/aura-install.sh
 
 # Installations
-sudo pacman -S $(awk '{print $1}' mypcks.lst)
+sudo pacman -S --needed $(awk '{print $1}' mypcks.lst)
 sudo aura -A $(awk '{print $1}'  mypcks-aura.lst)
 
 # Services
 sudo systemctl enable teamviewerd
-
-# Others
-sudo sed -i '/^txixco/ s/\/bin\/bash/\/usr\/bin\/zsh/g' /etc/passwd
-sudo sed -i '/[SSH]/,+3 s/22/486/' /etc/ufw/applications.d/ufw-loginserver
-sudo sed -i 's/#Port 22/Port 486/' /etc/ssh/sshd_config
-
-sudo systemctl restart sshd.service

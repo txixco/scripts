2fb98735227e0252b74de4633eb1e8ec2ce9a324
Francisco Rueda <fjrueda@west.com>
2022-04-29 10:14:29 -0500

    Added the class Player to CardGames/War

--- Python/CardGames/War/player.py
+++ Python/CardGames/War/player.py
@@ -4,13 +4,33 @@
 
 from typing import List
 
-from card import Card
+from card import Card, Rank, Suit
 
 
 class Player:
     """Class to store all the player information"""
 
     hand: List[Card]
+    name: str
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.hand = []
+
+    def __str__(self) -> str:
+        return f"{self.name} has {len(self.hand)} cards."
+
+    def discard(self) -> Card:
+        return self.hand.pop(0)
+
+    def take(self, new_cards: object) -> None:
+        if isinstance(new_cards, list):
+            for card in new_cards:
+                if not isinstance(card, Card):
+                    raise TypeError
+                
+            self.hand.extend(new_cards)
+        elif isinstance(new_cards, Card):
+            self.hand.append(new_cards)
+        else:
+            raise TypeError

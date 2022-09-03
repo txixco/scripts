e94314da668b19dcc02b1e4769ca01d2fbf5a3d8
Francisco Rueda <fruedadev@gmail.com>
2022-09-03 17:06:22 -0500

    Added the code for #5

---
+++ Haskell/retos/05-smallest.hs
@@ -0,0 +1,16 @@
+import Maths
+
+nums = [1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+divByAll :: Int -> [Int] -> Bool
+divByAll _ []     = True
+divByAll n (x:xs) = (isDivisible n x) && (divByAll n xs)
+
+smallest :: Int -> Int
+smallest n 
+    | (divByAll n nums) = n
+    | otherwise          = smallest (n+1)
+
+main :: IO()
+main = do
+    print (smallest 1)

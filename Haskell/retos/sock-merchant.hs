e002a48dc44f6fbc002f483914d5914ecbb9285c
Francisco Rueda <fruedadev@gmail.com>
2023-03-17 20:29:40 -0600

    Modified the Haskell's challenge sock-merchant

--- Haskell/retos/sock-merchant.hs
+++ Haskell/retos/sock-merchant.hs
@@ -3,15 +3,13 @@ import Data.List (group, sort)
 parse :: String -> Int
 parse = read
 
-groups :: [Int] -> [[Int]]
-groups = group . sort
-
-solution :: [[Int]] -> [Int]
-solution xs = [length ys `div` 2 | ys <- xs, length ys > 1]
+solution :: [Int] -> [Int]
+solution xs = [length ys `div` 2 | ys <- groups, length ys > 1]
+  where groups = group $ sort xs
 
 main :: IO()
 main = do
   _ <- getLine
   line <- getLine
   let input = map parse $ words line
-  print $ sum $ solution $ groups input
+  print $ sum $ solution input

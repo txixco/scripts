662b265ad48b41a8cb8657ab95242cad8586e278
txixco <frueda@protonmail.com>
2025-07-29 16:01:33 -0600

    Added the bookworm Telegram bot

--- Python/telepot/testtelepot.py
+++ Python/telepot/testtelepot.py
@@ -10,33 +10,33 @@ from time import sleep
 
 load_dotenv()
 
-TELEGRAM_TOKEN = getenv("TEST_TOKEN")
+TELEGRAM_TOKEN = getenv('TEST_TOKEN')
 if not TELEGRAM_TOKEN:
-	print("Error: TOKEN no encontrado.")
+	print('Error: TOKEN no encontrado.')
 	exit(1)
 
 bot: Bot = None
 visits: dict = {}
 
 def get_commands() -> list:
-	req = get("https://api.telegram.org/bot{}/getMyCommands".format(TELEGRAM_TOKEN))
+	req = get('https://api.telegram.org/bot{}/getMyCommands'.format(TELEGRAM_TOKEN))
 
 	data = req.json()
-	if data["ok"]:
-		return data["result"]
+	if data['ok']:
+		return data['result']
 	else:
-		print("Error en la respuesta de la API.")
+		print('Error en la respuesta de la API.')
 		return []
 
 def show_help(chat_id: int) -> None:
 	commands = get_commands()
 	if not commands:
-		bot.sendMessage(chat_id, "No hay comandos disponibles.")
+		bot.sendMessage(chat_id, 'No hay comandos disponibles.')
 		return
 
-	text = "🤖 Comandos disponibles:\n\n"
+	text = '🤖 Comandos disponibles:\n\n'
 	for command in commands:
-		text += f"    /{command['command']} - {command['description']}\n"
+		text += f'    /{command['command']} - {command['description']}\n'
 
 	bot.sendMessage(chat_id, text)
 
@@ -52,7 +52,7 @@ def obtain_fake_data(chat_id: str) -> dict:
 	typing_thread.start()
 
 	try:
-		req = get("https://randomuser.me/api/")
+		req = get('https://randomuser.me/api/')
 		if req.status_code == 200:
 			data = req.json()
 	finally:
@@ -63,24 +63,24 @@ def obtain_fake_data(chat_id: str) -> dict:
 
 def process_command(chat_id: int, text: str, who: dict) -> None:
 	match text:
-		case "/start":
-			bot.sendMessage(chat_id, f"¡Bienvenido, {who['first_name']}!")
+		case '/start':
+			bot.sendMessage(chat_id, f'¡Bienvenido, {who['first_name']}!')
 			show_help(chat_id)
-		case "/help":
+		case '/help':
 			show_help(chat_id)
-		case "/me":
-			bot.sendMessage(chat_id, f"Tu nombre es {who['first_name']} {who['last_name']}")
-		case "/fake":
+		case '/me':
+			bot.sendMessage(chat_id, f'Tu nombre es {who['first_name']} {who['last_name']}')
+		case '/fake':
 			obtained_data = obtain_fake_data(chat_id)
 			if obtained_data:
-				user = obtained_data["results"][0]
-				name = user["name"]
-				email = user["email"]
-				bot.sendMessage(chat_id, f"{name['first']} {name['last']}\n{email}")
+				user = obtained_data['results'][0]
+				name = user['name']
+				email = user['email']
+				bot.sendMessage(chat_id, f'{name['first']} {name['last']}\n{email}')
 			else:
 				bot.sendMessage(chat_id, obtained_data)
 		case _:
-			bot.sendMessage(chat_id, "Comando no reconocido.")
+			bot.sendMessage(chat_id, 'Comando no reconocido.')
 			show_help(chat_id)
 				
 def msg_loop(msg: str) -> None:
@@ -88,17 +88,17 @@ def msg_loop(msg: str) -> None:
 	global visits
 
 	content_type, chat_type, chat_id = glance(msg)
-	text = msg["text"]
-	who = msg["from"]
+	text = msg['text']
+	who = msg['from']
 	visits[chat_id] = visits.get(chat_id, 0) + 1
 	
-	if content_type == "text" and text.startswith("/"):
+	if content_type == 'text' and text.startswith('/'):
 		process_command(chat_id, text, who)
 	else:
 		bot.sendMessage(chat_id, text)
 	
 	if visits[chat_id] == 5:
-		bot.sendMessage(chat_id, "¡Por Cutie!, sí que te está gustando el bot 🦾")
+		bot.sendMessage(chat_id, '¡Por Cutie!, sí que te está gustando el bot 🦾')
 
 def main() -> None:
 	global bot
@@ -109,5 +109,5 @@ def main() -> None:
 	while True:
 		sleep(5)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
